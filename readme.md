34c4c3ecc3441729e049649d0984156040203b4e
pjc02478 <pjc0247@gmail.com>
2016-04-19 11:55:44 +0900

    Namu

---
+++ readme.md
@@ -0,0 +1,60 @@
+Namu
+====
+![mock](logo.png)<br>
+__(Namu-Mock)__
+
+목업 설정하기
+----
+__Static Method__
+```c#
+Mock.Method(() => MyClass.Method())
+  .Should(10);
+```
+
+__Method__
+```c#
+Mock.Select<MyClass>()
+  .Method(x => x.Method())
+  .Should(10);
+```
+
+__Property__
+```c#
+Mock.Select<MyClass>()
+  .Method(x => x.Property)
+  .Should(10);
+```
+
+목업된 값 가져오기
+----
+```c#
+int SomeMethod() {
+  if (Mock.IsRegistered) return (int)Mock.Value;
+}
+```
+`Namu`의 API들은 자동으로 caller 정보를 가져오기 때문에 호출자의 정보를 같이 넘겨줄 필요가 없습니다.
+
+스레드 안정성
+----
+기본적으로 `Namu`의 모든 public API들은 스레드에 안전합니다.<br>
+하지만 멀티스레드 환경에서는 여전히 아래와 같은 시나리오가 발생할 수 있기 때문에, `Namu`는 트랜젝션 API를 제공합니다.
+```
+IsRegistered -> True 로 인해서 IF 진입
+Mock.Unregister 
+Value 실행 시 값이 없으므로 익셉션 발생
+```
+아래의 예제들은 트랜젝션을 사용하여 전체 동작을 스레드에 안전하게 만드는 방법을 보여줍니다.
+```c#
+int SomeMethod() {
+  if (Mock.IsRegisteredTx) return Mock.Value;
+}
+```
+`IsRegisteredTx` API가 호출되면 트랜젝션이 시작됩니다. 다음번 `Value`가 실행될 때 까지 크리티컬 섹션에 진입한 상태로 남아있게 됨에 주의해 주세요.
+
+```c#
+int SomeMethod() {
+  int value;
+  if (Mock.TryGetValue(out value)) return value;
+}
+```
+또는 위와 같이 단일 API 호출로 대체할 수 있습니다.
---
+++ src/Namu.cs
@@ -0,0 +1,248 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Namu
+{
+    class ReflectionUtility
+    {
+        public static MethodBase Caller
+        {
+            get
+            {
+                StackFrame frame = new StackFrame(2);
+                return frame.GetMethod();
+            }
+        }
+        public static MethodBase Caller2
+        {
+            get
+            {
+                StackFrame frame = new StackFrame(3);
+                return frame.GetMethod();
+            }
+        }
+    }
+
+    public sealed class Mock
+    {
+        public static Selector<T> Select<T>()
+        {
+            return new Selector<T>();
+        }
+        public static Selector<T> Select<T>(T obj)
+        {
+            return new Selector<T>();
+        }
+
+        public static BoundMethod Method(Expression<Action> methodLambda)
+        {
+            MethodCallExpression call = methodLambda.Body as MethodCallExpression;
+            if (call == null)
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a property, not a method.",
+                    methodLambda.ToString()));
+
+            return new BoundMethod(call.Method);
+        }
+
+        public static bool IsRegistered
+        {
+            get
+            {
+                var caller = ReflectionUtility.Caller;
+                bool registered = false;
+
+                lock (mocks)
+                {
+                    registered = mocks.ContainsKey(GetPath(caller));
+                }
+
+                return registered;
+            }
+        }
+        public static bool IsRegisteredTx
+        {
+            get
+            {
+                var caller = ReflectionUtility.Caller;
+
+                // see https://msdn.microsoft.com/en-us/library/ms173179(v=vs.80).aspx
+                try
+                {
+                    Monitor.Enter(mocks);
+
+                    return mocks.ContainsKey(GetPath(caller));
+                }
+                catch
+                {
+                    Monitor.Exit(mocks);
+                }
+                return false;
+            }
+        }
+        public static object Value
+        {
+            get
+            {
+                if (Monitor.IsEntered(mocks))
+                    return ValueTx;
+
+                var caller = ReflectionUtility.Caller;
+
+                lock (mocks)
+                {
+                    return mocks[GetPath(caller)];
+                }
+            }
+        }
+        private static object ValueTx
+        {
+            get
+            {
+                try
+                {
+                    var caller = ReflectionUtility.Caller2;
+
+                    return mocks[GetPath(caller)];
+                }
+                finally
+                {
+                    Monitor.Exit(mocks);
+                }
+            }
+        }
+
+        public static bool TryGetValue(out object value)
+        {
+            var caller = ReflectionUtility.Caller;
+            var path = GetPath(caller);
+            value = null;
+
+            lock (mocks)
+            {
+                if (mocks.ContainsKey(path) == false)
+                    return false;
+
+                value = mocks[path];
+            }
+            return true;
+        }
+
+        private static Dictionary<string, object> mocks { get; set; }
+
+        static Mock()
+        {
+            mocks = new Dictionary<string, object>();
+        }
+
+        internal static string GetPath(MethodBase method)
+        {
+            return $"{method.DeclaringType.FullName}::{method.Name}";
+        }
+        internal static string GetPath(PropertyInfo property)
+        {
+            if (property.CanWrite)
+                throw new ArgumentException("Property is setter");
+            return $"{property.DeclaringType.FullName}::get_{property.Name}";
+        }
+
+        internal static void Bind(string path, object value)
+        {
+            lock (mocks)
+            {
+                mocks[path] = value;
+            }
+        }
+        internal static void Unbind(string path)
+        {
+            lock (mocks)
+            {
+                mocks.Remove(path);
+            }
+        }
+        public static void Claer()
+        {
+            lock (mocks)
+            {
+                mocks.Clear();
+            }
+        }
+    }
+
+    public sealed class Selector<T>
+    {
+        public Selector()
+        {
+        }
+        
+        public BoundProperty<T, TProperty> Property<TProperty>(Expression<Func<T, TProperty>> propertyLambda)
+        {
+            Type type = typeof(T);
+
+            MemberExpression member = propertyLambda.Body as MemberExpression;
+            if (member == null)
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a method, not a property.",
+                    propertyLambda.ToString()));
+
+            PropertyInfo property = member.Member as PropertyInfo;
+            if (property == null)
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a field, not a property.",
+                    propertyLambda.ToString()));
+
+            if (type != property.ReflectedType &&
+                !type.IsSubclassOf(property.ReflectedType))
+                throw new ArgumentException(string.Format(
+                    "Expresion '{0}' refers to a property that is not from type {1}.",
+                    propertyLambda.ToString(),
+                    type));
+
+            return new BoundProperty<T, TProperty>(property);
+        }
+        public BoundMethod Method(Expression<Action<T>> methodLambda)
+        {
+            MethodCallExpression call = methodLambda.Body as MethodCallExpression;
+            if (call == null)
+                throw new ArgumentException(string.Format(
+                    "Expression '{0}' refers to a property, not a method.",
+                    methodLambda.ToString()));
+
+            return new BoundMethod(call.Method);
+        }
+    }
+
+    public sealed class BoundProperty<T, TProperty>
+    {
+        private PropertyInfo property { get; set; }
+
+        public BoundProperty(PropertyInfo property)
+        {
+            this.property = property;
+        }
+
+        public void Should(object value)
+        {
+            Mock.Bind(Mock.GetPath(property), value);
+        }
+    }
+    public sealed class BoundMethod
+    {
+        private MethodInfo method { get; set; }
+
+        public BoundMethod(MethodInfo method)
+        {
+            this.method = method;
+        }
+
+        public void Should(object value)
+        {
+            Mock.Bind(Mock.GetPath(method), value);
+        }
+    }
+}
